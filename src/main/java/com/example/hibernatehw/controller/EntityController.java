45ea51f5727cb671817ef4e965ae159cacc44dea
Artem <riddle1994@ya.ru>
2021-10-31 18:25:54 +0300

    jpa added

--- src/main/java/com/example/hibernatehw/controller/EntityController.java
+++ src/main/java/com/example/hibernatehw/controller/EntityController.java
@@ -1,24 +1,36 @@
 package com.example.hibernatehw.controller;
 
 import com.example.hibernatehw.model.Person;
-import com.example.hibernatehw.service.EntityService;
+import com.example.hibernatehw.service.PersonCrudService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EntityController {
-    EntityService entityService;
+    private final PersonCrudService service;
 
-    public EntityController(EntityService entityService) {
-        this.entityService = entityService;
+    public EntityController(PersonCrudService service) {
+        this.service = service;
     }
 
     @GetMapping("/persons/by-city")
     public List<Person> getPersonsByCity(@RequestParam("city") String city) {
-        return entityService.getPersonsByCity(city);
+        return service.findByCityOfLiving(city);
+    }
+
+    @GetMapping("/persons/by-age")
+    public List<Person> getPersonsByAge(@RequestParam("age") int age) {
+        return service.findByAgeLessThanOrderBy(age);
+    }
+
+    @GetMapping("/persons/by-nameandsurname")
+    public Optional<Person> getPersonsByNameAndSurname(@RequestParam("name") String name,
+                                                       @RequestParam("surname") String surname) {
+        return service.findByNameAndSurname(name, surname);
     }
 
 }
